405f10ee27f6cba387ee9c6eff4ab3bc18d79a9f
6161990 <6161990src@gmail.com>
2021-03-15 07:51:34 +0900

    modified StarCoffee MESSAGE

--- src/ch5CT/StarCoffee.java
+++ src/ch5CT/StarCoffee.java
@@ -11,7 +11,7 @@ public class StarCoffee {
 		}else if (money == Menu.STARLATTE) {
 			return "별다방 라떼를 구입하셨습니다.";
 		}else if (money == Menu.STARFRAPPUCINO) {
-			return "별다방 푸라푸치노를 구입하셨습니다.";
+			return "별다방 프라푸치노를 구입하셨습니다.";
 		}else {
 			return null; 
 		}
